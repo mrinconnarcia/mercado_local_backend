5f0a3dda799244c444af00967aa91044471acf02
Martín Rincón <mrinconnarcia@gmail.com>
2026-09-18 22:29:55 -0600

    update config of cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:4200" # puerto default de Ember
+    origins "http://localhost:4000" # puerto default de Ember
 
     resource "*",
       headers: :any,
